4ee49053816fda825f02b4c624e8acd519ec7f02
Danilo Nobre <danilonobre@gmail.com>
2021-04-17 22:22:33 -0300

    vr case

--- gatsby-config.js
+++ gatsby-config.js
@@ -73,7 +73,7 @@ module.exports = {
           {
             resolve: `gatsby-remark-images`,
             options: {
-              maxWidth: 1000,
+              maxWidth: 2000,
               linkImagesToOriginal: false,
             },
           },
