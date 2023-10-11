97a8bebfbfd0187439af6d5c02fd6a190af38b61
Danilo Nobre <danilonobre@gmail.com>
2023-10-11 14:13:26 +0100

    figma component improvements e css updates for deprecated behaviors

--- src/components/figma.js
+++ src/components/figma.js
@@ -8,13 +8,13 @@ const Figma = ({url, urlMobile}) => {
 
             <div className="block-figma-mobile">
                 
-                <iframe src={"https://www.figma.com/embed?embed_host=danilonobre&url="+urlMobile+"&hide-ui=1"} allowfullscreen></iframe>  
+                <iframe src={"https://www.figma.com/embed?embed_host=danilonobre&url="+urlMobile+"&hide-ui=1&hotspot-hints=1&scaling=scale-down-width"} allowfullscreen></iframe>  
                 
             </div>
             
             <div className="block-figma">
                 
-                <iframe src={"https://www.figma.com/embed?embed_host=danilonobre&url="+url+"&hide-ui=1"} allowfullscreen></iframe>  
+                <iframe src={"https://www.figma.com/embed?embed_host=danilonobre&url="+url+"&hide-ui=1&hotspot-hints=1&scaling=scale-down-width"} allowfullscreen></iframe>  
                 
             </div>
         
